a760cf09e8ad980f6922bf147ed46e17184cd611
evyatarea <evyatarea@icloud.com>
2025-04-16 01:27:11 +0300

    Update project_form_gsheet.py

--- project_form_gsheet.py
+++ project_form_gsheet.py
@@ -17,8 +17,7 @@ CREDENTIALS_FILE = "credentials.json"
 @st.cache_resource
 def connect_to_gsheet():
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    service_account_info = json.loads(st.secrets["GOOGLE_CREDENTIALS"])
-    creds = Credentials.from_service_account_info(service_account_info, scopes=scope)
+    creds = Credentials.from_service_account_info(st.secrets["GOOGLE_CREDENTIALS"], scopes=scope)
     client = gspread.authorize(creds)
     sheet = client.open(GOOGLE_SHEET_NAME).sheet1
     return sheet
