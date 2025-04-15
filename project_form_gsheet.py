6306e8ba345e7038c1da1f8b92a8bd09c0d11d9f
evyatarea <evyatarea@icloud.com>
2025-04-16 01:19:12 +0300

    Update project_form_gsheet.py

--- project_form_gsheet.py
+++ project_form_gsheet.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import gspread
-import json
 from google.oauth2.service_account import Credentials
 from datetime import datetime
 
@@ -19,8 +18,7 @@ def connect_to_gsheet():
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive"
             ]
-    service_account_info = json.loads(st.secrets["GOOGLE_CREDENTIALS"])
-    creds = Credentials.from_service_account_info(service_account_info, scopes=scope)
+   st.write("📦 Loaded secret:", st.secrets["GOOGLE_CREDENTIALS"])
     client = gspread.authorize(creds)
     sheet = client.open(GOOGLE_SHEET_NAME).sheet1
     return sheet
