17c0a0ae7fc3f06c845304475fad0c01164e8ef4
evyatarea <evyatarea@icloud.com>
2025-04-16 23:26:10 +0300

    Update project_form_gsheet_fixed.py

--- project_form_gsheet_fixed.py
+++ project_form_gsheet_fixed.py
@@ -13,7 +13,7 @@ GOOGLE_SHEET_NAME = "Project Status Form"
 def connect_to_gsheet():
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
     service_account_info = json.loads(st.secrets["GOOGLE_CREDENTIALS"])
-    creds = Credentials.from_service_account_info(service_account_info, scopes=scope)
+    creds = Credentials.from_service_account_info(st.secrets["GOOGLE_CREDENTIALS"], scopes=scope)
     client = gspread.authorize(creds)
     sheet = client.open(GOOGLE_SHEET_NAME).sheet1
     return sheet
